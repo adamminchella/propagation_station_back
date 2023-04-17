a981f0c1bcef2af0142c30ecb3514d63c717f4c7
adamminchella <adamminchella@msn.com>
2023-04-17 17:46:06 +0100

    Initialise database connection with basic user model

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+from config import Config
+
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db = SQLAlchemy()
+db.init_app(app)
+
+migrate = Migrate(app, db)
 
 
 @app.route("/")
@@ -10,3 +21,5 @@ def index():
 
 if __name__ == "__main__":
     app.run()
+
+import models
